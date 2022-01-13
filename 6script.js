b426df1e178de61a9aef422a0717ce5b970c2539
calebmct <jmctwigan@gmail.com>
2022-01-13 09:30:17 -0500

    Update 6script.js
    wrapped jq in $(document).ready(function()) {
    });

--- 6script.js
+++ 6script.js
@@ -3,7 +3,7 @@ function newSecretWord() {
     return secretWord;
   }
 //Only allow letters (and backspace) to by typed -- https://stackoverflow.com/questions/2980038/allow-text-box-only-for-letters-using-jquery
-$(function(){
+$(document).ready(function(){
     $(".guess").on("keydown",function(event) {
         var arr = [8];
         for(var i = 65; i <= 90; i++){
@@ -16,11 +16,13 @@ $(function(){
 });
 
 //Only allow letters to be pasted
-$(".guess").on("input", function(){
-  var regexp = /[^a-zA-Z]/g;
-  if($(this).val().match(regexp)){
-    $(this).val( $(this).val().replace(regexp,'') );
-  }
+$(document).ready(function(){
+    $(".guess").on("input", function(){
+    var regexp = /[^a-zA-Z]/g;
+    if($(this).val().match(regexp)){
+        $(this).val( $(this).val().replace(regexp,'') );
+    }
+    });
 });
 
 var firstGuess = false;
@@ -30,22 +32,25 @@ var fourthGuess = false;
 var fifthGuess = false;
 var sixthGuess = false;
 
-$("#A").keyup(function () {
-	var uno = $(this).val().charAt(0).toUpperCase();
-  var dos = $(this).val().charAt(1).toUpperCase();
-  var tres = $(this).val().charAt(2).toUpperCase();
-  var cuatro = $(this).val().charAt(3).toUpperCase();
-  var cinco = $(this).val().charAt(4).toUpperCase();
-	$("#A1").val(uno);
-  $("#A2").val(dos);
-  $("#A3").val(tres);
-  $("#A4").val(cuatro);
-  $("#A5").val(cinco);
+$(document).ready(function(){
+    $("#A").keyup(function () {
+        var uno = $(this).val().charAt(0).toUpperCase();
+    var dos = $(this).val().charAt(1).toUpperCase();
+    var tres = $(this).val().charAt(2).toUpperCase();
+    var cuatro = $(this).val().charAt(3).toUpperCase();
+    var cinco = $(this).val().charAt(4).toUpperCase();
+        $("#A1").val(uno);
+    $("#A2").val(dos);
+    $("#A3").val(tres);
+    $("#A4").val(cuatro);
+    $("#A5").val(cinco);
+    });
 });
-
-$("#validateGuessA").click(function() {
-  this.disabled = true;
-  $("#A").prop('disabled',true);
+$(document).ready(function(){
+    $("#validateGuessA").click(function() {
+    this.disabled = true;
+    $("#A").prop('disabled',true);
+    });
 });
 
 function validateA() {
