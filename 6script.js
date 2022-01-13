89629ba7262818869dd5bcf120a7477a35a1a595
calebmct <jmctwigan@gmail.com>
2022-01-13 10:58:39 -0500

    Update 6script.js
    removed 1 wrapped function on test function

--- 6script.js
+++ 6script.js
@@ -53,19 +53,17 @@ $(document).ready(function(){
     });
 });
 $(document).ready(function(){
-    function validateA() {
-        var validateA1 = document.getElementById("#A1").value.charAt(0);
-        var validateA2 = document.getElementById("#A2").value.charAt(1);
-        var validateA3 = document.getElementById("#A3").value.charAt(2);
-        var validateA4 = document.getElementById("#A4").value.charAt(3);
-        var validateA5 = document.getElementById("#A5").value.charAt(4);
-        var validateSw1 = newSecretWord().value.charAt(0);
-        var validateSw2 = newSecretWord().value.charAt(1);
-        var validateSw3 = newSecretWord().value.charAt(2);
-        var validateSw4 = newSecretWord().value.charAt(3);
-        var validateSw5 = newSecretWord().value.charAt(4);
-        $("#validateGuessA").click(function(){
-            $(#testValidate).val("TRUTH");
-        });
-    };
+    var validateA1 = document.getElementById("#A1").value.charAt(0);
+    var validateA2 = document.getElementById("#A2").value.charAt(1);
+    var validateA3 = document.getElementById("#A3").value.charAt(2);
+    var validateA4 = document.getElementById("#A4").value.charAt(3);
+    var validateA5 = document.getElementById("#A5").value.charAt(4);
+    var validateSw1 = newSecretWord().value.charAt(0);
+    var validateSw2 = newSecretWord().value.charAt(1);
+    var validateSw3 = newSecretWord().value.charAt(2);
+    var validateSw4 = newSecretWord().value.charAt(3);
+    var validateSw5 = newSecretWord().value.charAt(4);
+    $("#validateGuessA").click(function(){
+        $(#testValidate).val("TRUTH");
+    });
 });
